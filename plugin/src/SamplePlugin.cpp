0c7b29b50c731ad6a9cbf469308c666abe735995
Sampuka <matddi@gmail.com>
2020-04-13 22:19:26 +0200

    Initial for plugin

---
+++ plugin/src/SamplePlugin.cpp
@@ -0,0 +1,346 @@
+#include "SamplePlugin.hpp"
+
+#include <cstdlib>
+
+SamplePlugin::SamplePlugin():
+    RobWorkStudioPlugin("SamplePluginUI", QIcon(":/pa_icon.png"))
+{
+    setupUi(this);
+
+    _timer = new QTimer(this);
+    connect(_timer, SIGNAL(timeout()), this, SLOT(timer()));
+
+    // now connect stuff from the ui component
+    connect(_btn_im    ,SIGNAL(pressed()), this, SLOT(btnPressed()) );
+    connect(_btn_scan    ,SIGNAL(pressed()), this, SLOT(btnPressed()) );
+    connect(_btn0    ,SIGNAL(pressed()), this, SLOT(btnPressed()) );
+    connect(_btn1    ,SIGNAL(pressed()), this, SLOT(btnPressed()) );
+    connect(_spinBox  ,SIGNAL(valueChanged(int)), this, SLOT(btnPressed()) );
+
+    _framegrabber = NULL;
+
+    _cameras = {"Camera_Right", "Camera_Left"};
+    _cameras25D = {"Scanner25D"};
+}
+
+SamplePlugin::~SamplePlugin()
+{
+    delete _textureRender;
+    delete _bgRender;
+}
+
+void SamplePlugin::initialize()
+{
+    log().info() << "INITALIZE" << "\n";
+
+    getRobWorkStudio()->stateChangedEvent().add(std::bind(&SamplePlugin::stateChangedListener, this, std::placeholders::_1), this);
+
+    // Get path to project from environment
+    char* projectpath = std::getenv("DUALROBOTDIR");
+
+    if (projectpath == NULL)
+    {
+        std::cerr << "DUALROBOTDIR environment variable not set! Perform \"export DUALROBOTDIR=/home/user/dual-robots/\", with the correct path." << std::endl;
+    }
+    else
+    {
+        WorkCell::Ptr wc = WorkCellLoader::Factory::load(std::string(projectpath) + "/workcell/Scene.wc.xml");
+
+        if (wc == nullptr)
+        {
+            std::cerr << "Unable to autoload workcell! Maybe DUALROBOTDIR environment variable not set correctly?" << std::endl;
+        }
+        else
+        {
+            getRobWorkStudio()->setWorkCell(wc);
+        }
+    }
+}
+
+void SamplePlugin::open(WorkCell* workcell)
+{
+    log().info() << "OPEN" << "\n";
+    _wc = workcell;
+    _state = _wc->getDefaultState();
+
+    log().info() << workcell->getFilename() << "\n";
+
+    if (_wc != NULL)
+    {
+        // Add the texture render to this workcell if there is a frame for texture
+        Frame* textureFrame = _wc->findFrame("MarkerTexture");
+        if (textureFrame != NULL)
+        {
+            getRobWorkStudio()->getWorkCellScene()->addRender("TextureImage",_textureRender,textureFrame);
+        }
+        // Add the background render to this workcell if there is a frame for texture
+        Frame* bgFrame = _wc->findFrame("Background");
+        if (bgFrame != NULL)
+        {
+            getRobWorkStudio()->getWorkCellScene()->addRender("BackgroundImage",_bgRender,bgFrame);
+        }
+
+        // Create a GLFrameGrabber if there is a camera frame with a Camera property set
+        Frame* cameraFrame = _wc->findFrame(_cameras[0]);
+        if (cameraFrame != NULL)
+        {
+            if (cameraFrame->getPropertyMap().has("Camera"))
+            {
+                // Read the dimensions and field of view
+                double fovy;
+                int width,height;
+                std::string camParam = cameraFrame->getPropertyMap().get<std::string>("Camera");
+                std::istringstream iss (camParam, std::istringstream::in);
+                iss >> fovy >> width >> height;
+                // Create a frame grabber
+                _framegrabber = new GLFrameGrabber(width,height,fovy);
+                SceneViewer::Ptr gldrawer = getRobWorkStudio()->getView()->getSceneViewer();
+                _framegrabber->init(gldrawer);
+            }
+        }
+
+        Frame* cameraFrame25D = _wc->findFrame(_cameras25D[0]);
+        if (cameraFrame25D != NULL)
+        {
+            if (cameraFrame25D->getPropertyMap().has("Scanner25D"))
+            {
+                // Read the dimensions and field of view
+                double fovy;
+                int width,height;
+                std::string camParam = cameraFrame25D->getPropertyMap().get<std::string>("Scanner25D");
+                std::istringstream iss (camParam, std::istringstream::in);
+                iss >> fovy >> width >> height;
+                // Create a frame grabber
+                _framegrabber25D = new GLFrameGrabber25D(width,height,fovy);
+                SceneViewer::Ptr gldrawer = getRobWorkStudio()->getView()->getSceneViewer();
+                _framegrabber25D->init(gldrawer);
+            }
+        }
+
+        _device = _wc->findDevice("UR-6-85-5-A");
+        _step = -1;
+    }
+}
+
+
+void SamplePlugin::close()
+{
+    log().info() << "CLOSE" << "\n";
+
+    // Stop the timer
+    _timer->stop();
+    // Remove the texture render
+    Frame* textureFrame = _wc->findFrame("MarkerTexture");
+    if (textureFrame != NULL)
+    {
+        getRobWorkStudio()->getWorkCellScene()->removeDrawable("TextureImage",textureFrame);
+    }
+    // Remove the background render
+    Frame* bgFrame = _wc->findFrame("Background");
+    if (bgFrame != NULL)
+    {
+        getRobWorkStudio()->getWorkCellScene()->removeDrawable("BackgroundImage",bgFrame);
+    }
+    // Delete the old framegrabber
+    if (_framegrabber != NULL)
+    {
+        delete _framegrabber;
+    }
+    _framegrabber = NULL;
+    _wc = NULL;
+}
+
+Mat SamplePlugin::toOpenCVImage(const Image& img)
+{
+    Mat res(img.getHeight(),img.getWidth(), CV_8SC3);
+    res.data = (uchar*)img.getImageData();
+    return res;
+}
+
+
+void SamplePlugin::btnPressed()
+{
+    QObject *obj = sender();
+    if(obj==_btn0)
+    {
+        //		log().info() << "Button 0\n";
+        //		// Toggle the timer on and off
+        //		if (!_timer25D->isActive())
+        //		    _timer25D->start(100); // run 10 Hz
+        //		else
+        //			_timer25D->stop();
+        _timer->stop();
+        rw::math::Math::seed();
+        double extend = 0.05;
+        double maxTime = 60;
+        Q from(6, 1.571, -1.572, -1.572, -1.572, 1.571, 0);
+        Q to(6, 1.847, -2.465, -1.602, -0.647, 1.571, 0); //From pose estimation
+        createPathRRTConnect(from, to, extend, maxTime);
+    }
+    else if(obj==_btn1)
+    {
+        log().info() << "Button 1\n";
+        // Toggle the timer on and off
+        if (!_timer->isActive())
+        {
+            _timer->start(100); // run 10 Hz
+            _step = 0;
+        }
+        else
+            _step = 0;
+    }
+    else if(obj==_spinBox)
+    {
+        log().info() << "spin value:" << _spinBox->value() << "\n";
+    }
+    else if( obj==_btn_im )
+    {
+        getImage();
+    }
+    else if( obj==_btn_scan )
+    {
+        get25DImage();
+    }
+}
+
+void SamplePlugin::get25DImage()
+{
+    if (_framegrabber25D != NULL)
+    {
+        for( int i = 0; i < _cameras25D.size(); i++)
+        {
+            // Get the image as a RW image
+            Frame* cameraFrame25D = _wc->findFrame(_cameras25D[i]); // "Camera");
+            _framegrabber25D->grab(cameraFrame25D, _state);
+
+            //const Image& image = _framegrabber->getImage();
+
+            const rw::geometry::PointCloud* img = &(_framegrabber25D->getImage());
+
+            std::ofstream output(_cameras25D[i] + ".pcd");
+            output << "# .PCD v.5 - Point Cloud Data file format\n";
+            output << "FIELDS x y z\n";
+            output << "SIZE 4 4 4\n";
+            output << "TYPE F F F\n";
+            output << "WIDTH " << img->getWidth() << "\n";
+            output << "HEIGHT " << img->getHeight() << "\n";
+            output << "POINTS " << img->getData().size() << "\n";
+            output << "DATA ascii\n";
+            for(const auto &p_tmp : img->getData())
+            {
+                rw::math::Vector3D<float> p = p_tmp;
+                output << p(0) << " " << p(1) << " " << p(2) << "\n";
+            }
+            output.close();
+        }
+    }
+}
+
+void SamplePlugin::getImage()
+{
+    if (_framegrabber != NULL)
+    {
+        for( int i = 0; i < _cameras.size(); i++)
+        {
+            // Get the image as a RW image
+            Frame* cameraFrame = _wc->findFrame(_cameras[i]); // "Camera");
+            _framegrabber->grab(cameraFrame, _state);
+
+            const rw::sensor::Image* rw_image = &(_framegrabber->getImage());
+
+            // Convert to OpenCV matrix.
+            cv::Mat image = cv::Mat(rw_image->getHeight(), rw_image->getWidth(), CV_8UC3, (rw::sensor::Image*)rw_image->getImageData());
+
+            // Convert to OpenCV image
+            Mat imflip, imflip_mat;
+            cv::flip(image, imflip, 1);
+            cv::cvtColor( imflip, imflip_mat, COLOR_RGB2BGR );
+
+            cv::imwrite(_cameras[i] + ".png", imflip_mat );
+
+            // Show in QLabel
+            QImage img(imflip.data, imflip.cols, imflip.rows, imflip.step, QImage::Format_RGB888);
+            QPixmap p = QPixmap::fromImage(img);
+            unsigned int maxW = 480;
+            unsigned int maxH = 640;
+            _label->setPixmap(p.scaled(maxW,maxH,Qt::KeepAspectRatio));
+        }
+    }
+}
+
+void SamplePlugin::timer()
+{
+    if(0 <= _step && _step < _path.size())
+    {
+        _device->setQ(_path.at(_step),_state);
+        getRobWorkStudio()->setState(_state);
+        _step++;
+    }
+}
+
+void SamplePlugin::stateChangedListener(const State& state)
+{
+    _state = state;
+}
+
+bool SamplePlugin::checkCollisions(Device::Ptr device, const State &state, const CollisionDetector &detector, const Q &q) {
+    State testState;
+    CollisionDetector::QueryResult data;
+    bool colFrom;
+
+    testState = state;
+    device->setQ(q,testState);
+    colFrom = detector.inCollision(testState,&data);
+    if (colFrom)
+    {
+        cerr << "Configuration in collision: " << q << endl;
+        cerr << "Colliding frames: " << endl;
+        FramePairSet fps = data.collidingFrames;
+        for (FramePairSet::iterator it = fps.begin(); it != fps.end(); it++)
+        {
+            cerr << (*it).first->getName() << " " << (*it).second->getName() << endl;
+        }
+        return false;
+    }
+    return true;
+}
+
+void SamplePlugin::createPathRRTConnect(Q from, Q to,  double extend, double maxTime)
+{
+    _device->setQ(from,_state);
+    getRobWorkStudio()->setState(_state);
+    CollisionDetector detector(_wc, ProximityStrategyFactory::makeDefaultCollisionStrategy());
+    PlannerConstraint constraint = PlannerConstraint::make(&detector,_device,_state);
+    QSampler::Ptr sampler = QSampler::makeConstrained(QSampler::makeUniform(_device),constraint.getQConstraintPtr());
+    QMetric::Ptr metric = MetricFactory::makeEuclidean<Q>();
+    QToQPlanner::Ptr planner = RRTPlanner::makeQToQPlanner(constraint, sampler, metric, extend, RRTPlanner::RRTConnect);
+
+    _path.clear();
+    if (!checkCollisions(_device, _state, detector, from))
+        cout << from << " is in colission!" << endl;
+    if (!checkCollisions(_device, _state, detector, to))
+        cout << to << " is in colission!" << endl;
+    Timer t;
+    t.resetAndResume();
+    planner->query(from,to,_path,maxTime);
+    t.pause();
+
+    if (t.getTime() >= maxTime)
+    {
+        cout << "Notice: max time of " << maxTime << " seconds reached." << endl;
+    }
+
+    const int duration = 10;
+
+    if(_path.size() == 2)
+    {   // The interpolated path between Q start and Q goal is collision free. Set the duration with respect to the desired velocity
+        LinearInterpolator<Q> linInt(from, to, duration);
+        QPath tempQ;
+        for(int i = 0; i < duration+1; i++)
+        {
+            tempQ.push_back(linInt.x(i));
+        }
+
+        _path=tempQ;
+    }
+}
---
+++ plugin/src/SamplePlugin.hpp
@@ -0,0 +1,111 @@
+#ifndef SAMPLEPLUGIN_HPP
+#define SAMPLEPLUGIN_HPP
+
+// RobWork includes
+#include <rw/models/WorkCell.hpp>
+#include <rw/kinematics/State.hpp>
+#include <rwlibs/opengl/RenderImage.hpp>
+#include <rwlibs/simulation/GLFrameGrabber.hpp>
+#include <rwlibs/simulation/GLFrameGrabber25D.hpp>
+
+#include <rw/rw.hpp>
+#include <rwlibs/pathplanners/rrt/RRTPlanner.hpp>
+#include <rwlibs/pathplanners/rrt/RRTQToQPlanner.hpp>
+#include <rwlibs/proximitystrategies/ProximityStrategyFactory.hpp>
+#include <rw/trajectory/LinearInterpolator.hpp>
+
+
+// RobWorkStudio includes
+#include <RobWorkStudioConfig.hpp> // For RWS_USE_QT5 definition
+#include <rws/RobWorkStudioPlugin.hpp>
+
+// OpenCV 3
+#include <opencv2/opencv.hpp>
+
+// Qt
+#include <QTimer>
+
+#include "ui_SamplePlugin.h"
+
+#include <rws/RobWorkStudio.hpp>
+
+#include <QPushButton>
+
+#include <rw/loaders/ImageLoader.hpp>
+#include <rw/loaders/WorkCellFactory.hpp>
+
+#include <functional>
+
+
+using namespace rw::common;
+using namespace rw::graphics;
+using namespace rw::kinematics;
+using namespace rw::loaders;
+using namespace rw::models;
+using namespace rw::sensor;
+using namespace rwlibs::opengl;
+using namespace rwlibs::simulation;
+
+using namespace std;
+using namespace rw::math;
+using namespace rw::pathplanning;
+using namespace rw::proximity;
+using namespace rw::trajectory;
+using namespace rwlibs::pathplanners;
+using namespace rwlibs::proximitystrategies;
+
+
+using namespace rws;
+
+using namespace cv;
+
+using namespace std::placeholders;
+
+
+class SamplePlugin: public rws::RobWorkStudioPlugin, private Ui::SamplePlugin
+{
+    Q_OBJECT
+    Q_INTERFACES( rws::RobWorkStudioPlugin )
+    Q_PLUGIN_METADATA(IID "dk.sdu.mip.Robwork.RobWorkStudioPlugin/0.1" FILE "plugin.json")
+    
+    public:
+        SamplePlugin();
+        virtual ~SamplePlugin();
+
+        virtual void open(rw::models::WorkCell* workcell);
+
+        virtual void close();
+
+        virtual void initialize();
+
+        private slots:
+            void btnPressed();
+        void timer();
+        void getImage();
+        void get25DImage();
+
+        void stateChangedListener(const rw::kinematics::State& state);
+
+        bool checkCollisions(Device::Ptr device, const State &state, const CollisionDetector &detector, const Q &q);
+        void createPathRRTConnect(Q from, Q to,  double extend, double maxTime);
+
+    private:
+        static cv::Mat toOpenCVImage(const rw::sensor::Image& img);
+
+        QTimer* _timer;
+        QTimer* _timer25D;
+
+        rw::models::WorkCell::Ptr _wc;
+        rw::kinematics::State _state;
+        rwlibs::opengl::RenderImage *_textureRender, *_bgRender;
+        rwlibs::simulation::GLFrameGrabber* _framegrabber;
+        rwlibs::simulation::GLFrameGrabber25D* _framegrabber25D;    
+        std::vector<std::string> _cameras;
+        std::vector<std::string> _cameras25D;
+        Device::Ptr _device;
+        QPath _path;
+        int _step;
+
+};
+
+#endif /*RINGONHOOKPLUGIN_HPP_*/
