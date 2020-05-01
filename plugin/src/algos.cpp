c8b47947c36893ee31c6bea3877944899cd9a633
sampuka <matddi@gmail.com>
2020-05-01 18:05:49 +0200

    deleted shitty shit fuck

--- plugin/src/algos.cpp
+++
@@ -1,7 +0,0 @@
-#include "algos.hpp"
-
-void findConstrainedTree(rwlibs::pathplanners::RRTTree<rw::math::Q> &tree, rw::models::Device::CPtr device)
-{
-    rw::math::Q newQ(1, 1, 1, 1, 1, 1);
-    tree.add(newQ, &tree.getRoot());
-}
